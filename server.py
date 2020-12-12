0a3f187b4b05d23c163108a198d87d8505748798
hussain0613 <hussain0613@gmail.com>
2020-12-12 07:20:22 +0600

    made some changes to make all of the commands work

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-if __name__ == '__main__':
-    from app import create_app
-    app = create_app()
-    app.run(debug=True, threaded = True)
-else:
-    from app import create_app
-    app = create_app()
+#if __name__ == '__main__':
+#    from .app import create_app
+#    app = create_app()
+#    app.run(debug=True, threaded = True)
+#else:
+#    from app import create_app
+#    app = create_app()
+
+from app import create_app
+app = create_app()
